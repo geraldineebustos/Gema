aa7f2d2685e25dfffb75d38e691bc777b1ca9a15
geraldineebustos <geraldinebustos121@gmail.com>
2026-09-16 21:02:11 -0300

    Create productos.php

---
+++ tp-9/productos.php
@@ -0,0 +1,9 @@
+<?php
+
+$productos = [
+    [ 'id' => 1, 'name' => 'Camiseta de futbol', 'price' => 15000 ],
+    [ 'id' => 2, 'name' => 'Botines',            'price' => 45000 ],
+    [ 'id' => 3, 'name' => 'Pelota',             'price' => 2000  ],
+    [ 'id' => 4, 'name' => 'Medias',             'price' => 3500  ],
+    [ 'id' => 5, 'name' => 'Guantes de arquero', 'price' => 12000 ],
+];
